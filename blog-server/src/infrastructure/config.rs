5d2c944e32ad2d5e038439cb45ccb55d26a9f976
Prorok89 <>
2026-04-01 23:25:07 +0300

    jwt 0.1

--- blog-server/src/infrastructure/config.rs
+++ blog-server/src/infrastructure/config.rs
@@ -5,14 +5,25 @@ pub struct Config {
     pub database_url: String,
     pub port: u16,
     pub debug: bool,
+    pub jwt_secret: String,
 }
 
 impl Config {
     pub fn from_env() -> Result<Self, BlogError> {
         let database_url = std::env::var("DATABASE_URL")?;
-        let port = std::env::var("PORT")?.parse::<u16>().map_err(|e|BlogError::ErrorNotKnow(e.to_string()))?;
-        let debug = std::env::var("DEBUG")?.parse::<bool>().map_err(|e|BlogError::ErrorNotKnow(e.to_string()))?;
+        let jwt_secret = std::env::var("JWT_SECRET")?;
+        let port = std::env::var("PORT")?
+            .parse::<u16>()
+            .map_err(|e| BlogError::ErrorNotKnow(e.to_string()))?;
+        let debug = std::env::var("DEBUG")?
+            .parse::<bool>()
+            .map_err(|e| BlogError::ErrorNotKnow(e.to_string()))?;
 
-        Ok(Self { database_url, port, debug })
+        Ok(Self {
+            database_url,
+            port,
+            debug,
+			jwt_secret
+        })
     }
 }
--- blog-server/src/infrastructure/jwt.rs
+++ blog-server/src/infrastructure/jwt.rs
@@ -1,18 +1,67 @@
-use chrono::{DateTime, Utc};
+use actix_web::dev::{ServiceRequest, Transform};
+use chrono::{Duration, Utc};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, decode, encode};
+use serde::{Deserialize, Serialize};
 
+use crate::domain::error::BlogError;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Claims {
     user_id: u64,
     username: String,
-    exp: DateTime<Utc>,
+    exp: usize,
 }
 
 struct JwtService {
-    encoding: String,
-    decoding: String,
+    encoding: EncodingKey,
+    decoding: DecodingKey,
 }
 
 impl JwtService {
-    pub fn new(secret: &str) {}
-    pub fn generate_token(user_id: u64, username: String) {}
-    pub fn verify_token(token: String) {}
+    pub fn new(secret: &str) -> Self {
+        Self {
+            encoding: EncodingKey::from_secret(secret.as_bytes()),
+            decoding: DecodingKey::from_secret(secret.as_bytes()),
+        }
+    }
+
+    pub fn generate_token(&self, user_id: u64, username: String) -> Result<String, BlogError> {
+        let cur_time = Utc::now();
+        let exp = cur_time + Duration::hours(24);
+
+        let claims = Claims {
+            user_id,
+            username,
+            exp : exp.timestamp() as usize,
+        };
+
+        let token = encode(&Header::default(), &claims, &self.encoding).map_err(|e| BlogError::ErrorNotKnow(e.to_string()))?;
+
+		Ok(token)
+    }
+
+    pub fn verify_token(&self, token: String) -> Result<Claims, BlogError>{
+		let mut validaion = Validation::default();
+		validaion.validate_exp = true;
+
+		let token_data = decode::<Claims> (token, &self.decoding, &validaion).map_err(|e| BlogError::ErrorNotKnow(e.to_string()))?;
+
+		Ok(token_data.claims)
+	}
+}
+
+impl<S, B> Transform<S, ServiceRequest> for  JwtService {
+	type Response;
+
+	type Error;
+
+	type Transform;
+
+	type InitError;
+
+	type Future;
+
+	fn new_transform(&self, service: S) -> Self::Future {
+		todo!()
+	}
 }
